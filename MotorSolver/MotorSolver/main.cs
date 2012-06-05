ccd217bb34751e0b25f533c1004f0e489f883fda
Matthew D Schechtman <matthew.d.schechtman@gmail.com>
2012-06-05 13:56:00 -0400

    created solution, folders, etc

---
+++ MotorSolver/MotorSolver/main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotorSolver
+{
+    class main
+    {
+        /*
+         * In the project properties, in the Application tab, there's a setting called "Output type". 
+         * If this is set to "Console Application", a command-prompt window will appear when running 
+         * the program, as well as the form. Setting it to "Windows Application" (the default for 
+         * Windows Forms projects) will sort this out.
+         * 
+         * This setting corresponds to the "/target" switch for the compiler. 
+         * /target:exe will give you the command-prompt, 
+         * /target:winexe will just show the form.
+         */
+        static int Main(String[] args)
+        {
+            return 0;
+        }
+    }
+}
