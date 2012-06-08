20f6a5ae5afe52da9b280a3089c233f26ca0e57f
Matthew D Schechtman <matthew.d.schechtman@gmail.com>
2012-06-08 13:57:50 -0400

    renamed solution, added additional C# projects, add state

---
+++ AerotechMotorSolver/Program.Test/Testing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Program;
+
+namespace Program.Test
+{
+    public class Testing
+    {
+        class main
+        {
+            static int Main(String[] args)
+            {
+                return 0;
+            }
+        }
+    }
+}
