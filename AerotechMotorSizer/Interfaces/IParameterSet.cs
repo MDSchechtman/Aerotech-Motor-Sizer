d526abe4a927a4d9813d6b59dc939ba4c31ae8a4
Matthew Schechtman <matthew.d.schechtman@gmail.com>
2012-06-22 14:31:32 -0400

    This is a working commit. Reworking how ParameterSet functions

--- AerotechMotorSizer/Interfaces/IParameterSet.cs
+++ AerotechMotorSizer/Interfaces/IParameterSet.cs
@@ -7,135 +7,36 @@ namespace Interfaces
 {
     interface IParameterSet
     {
-        double[] Position { get; }
+        double[] Axis1_Position { get; }
+        double[] Axis2_Position { get; }
+        double[] Axis3_Position { get; }
+
+        double Axis1_AccelerationTime { get; }
+        double Axis1_TraverseTime { get; }
+        double Axis1_DeccelerationTime { get; }
+        double Axis1_DwelTime { get; }
+
+        double Axis2_AccelerationTime { get; }
+        double Axis2_TraverseTime { get; }
+        double Axis2_DeccelerationTime { get; }
+        double Axis2_DwelTime { get; }
+
+        double Axis3_AccelerationTime { get; }
+        double Axis3_TraverseTime { get; }
+        double Axis3_DeccelerationTime { get; }
+        double Axis3_DwelTime { get; }
+
         double[] Time { get; }
 
-        /// <summary>
-        /// The Acceleration Time
-        /// </summary>
-        double AccelerationTime { get; }
 
-        /// <summary>
-        /// The Traverse Time
-        /// </summary>
-        double TraverseTime { get; }
-
-        /// <summary>
-        /// The Decceleration Time
-        /// </summary>
-        double DeccelerationTime { get; }
-
-        /// <summary>
-        /// The Dwel Time
-        /// </summary>
-        double DwelTime { get; }
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="distance">The distance the motor will travel</param>
-        /// <param name="totalTime">The total time to travel</param>
-        /// <param name="percentage">The percentage of time spent moving</param>
-        /// <returns></returns>
-        bool SetParametersDTP(double distance, double totalTime, double percentage);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="distance">The distance the motor will travel</param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="percentage">The percentage of time spent moving</param>
-        /// <returns></returns>
-        bool SetParametersDVP(double distance, double maxVelocity, double percentage);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="distance">The distance the motor will travel</param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="totalTime">The total time to travel</param>
-        /// <returns></returns>
-        bool SetParametersDMT(double distance, double maxVelocity, double totalTime);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="distance">The distance the motor will travel</param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="peakAcceleration"></param>
-        /// <returns></returns>
-        bool SetParametersDVA(double distance, double maxVelocity, double peakAcceleration);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="accelerationDistance"></param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="totalTravel"></param>
-        /// <returns></returns>
-        bool SetParametersAVT(double accelerationDistance, double maxVelocity, double totalTravel);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="accelerationDistance"></param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="totalTime">The total time to travel</param>
-        /// <returns></returns>
-        bool SetParametersAVT2(double accelerationDistance, double maxVelocity, double totalTime);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="peakAcceleration"></param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="maxTravel"></param>
-        /// <returns></returns>
-        bool SetParametersAVT3(double peakAcceleration, double maxVelocity, double maxTravel);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="peakAcceleration"></param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="totalTime">The total time to travel</param>
-        /// <returns></returns>
-        bool SetParametersAVT4(double peakAcceleration, double maxVelocity, double totalTime);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="peakAcceleration"></param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="maxTravel"></param>
-        /// <returns></returns>
-        bool SetParametersAVT5(double peakAcceleration, double maxVelocity, double maxTravel);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="peakAcceleration"></param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="scanDistance"></param>
-        /// <returns></returns>
-        bool SetParametersAVD(double peakAcceleration, double maxVelocity, double scanDistance);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="totalTravel"></param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="scanDistance"></param>
-        /// <returns></returns>
-        bool SetParametersTVD(double totalTravel, double maxVelocity, double scanDistance);
-
-        /// <summary>
-        /// Set the parameters for the solver
-        /// </summary>
-        /// <param name="totalTime">The total time to travel</param>
-        /// <param name="maxVelocity">The max velocity obtained during motion</param>
-        /// <param name="scanDistance"></param>
-        /// <returns></returns>
-        bool SetParametersTVD2(double totalTime, double maxVelocity, double scanDistance);
+        // Distance of travel
+        // Total time of travel
+        // Percentage of time spent moving
+        // Max velocity
+        // Peak acceleration
+        // Acceleration distance
+        // Max travel
+        // Scan distance
+        void SetParameters(Dictionary<int, double> parameters);
     }
 }
---
+++ AerotechMotorSizer/ParameterSet/ParameterSet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace ParameterSet
+{
+    public class ParameterSet
+    {
+        public ParameterSet()
+        {
+        }
+
+        // Get the string name and double value from each key in the dictionary
+        private bool getKey(Dictionary<string, double> p, int i, out string s, out double v)
+        {
+            int index = 0;
+            foreach (KeyValuePair<string, double> k in p)
+            {
+                if (index == i)
+                {
+                    s = k.Key; v = k.Value;
+                    return true;
+                }
+                i++;
+            }
+            s = string.Empty; v = 0;
+            return false;
+        }
+
+        // Given a dictionary of parameters, find and invoke the proper conversion
+        // method to get the necessary values for solving
+        public void SetParameters(Dictionary<string, double> parameters)
+        {
+            string param0 = string.Empty;
+            string param1 = string.Empty;
+            string param2 = string.Empty;
+
+            double value0 = 0;
+            double value1 = 0;
+            double value2 = 0;
+
+            // Get the paramets, checking for errors
+            bool zero = getKey(parameters, 0, out param0, out value0);
+            bool one = getKey(parameters, 0, out param1, out value1);
+            bool two = getKey(parameters, 0, out param2, out value2);
+
+            if (!(zero && one && two))
+                throw new Exception("Parameters could not be determined!");
+
+            // Invoke the proper method based on the input parameters
+            string invokeString = string.Format("{0}_{1}_{2}_converter", param0, param1, param2);
+            MethodInfo info = typeof(ParameterSet).GetMethod(invokeString);
+            info.Invoke(null, new object[] { value0, value1, value2 });
+        }
+
+        /* parameters:
+            distanceOfTravel,   // Distance of travel
+            totalTime,          // Total time of travel
+            percentage,         // Percentage of time spent moving
+            maxVelocity,        // Max velocity
+            peakAcceleratiin,   // Peak acceleration
+            accelDistance,      // Acceleration distance
+            maxTravel,          // Max travel
+            scanDistance        // Scan distance
+         */
+    }
+}
