7da7e0de26589b42b2a8841f0acf646fe7e7a19b
Matthew Schechtman <matthew.d.schechtman@gmail.com>
2012-06-28 17:30:23 -0400

    Added empty implemtations to converters

--- AerotechMotorSizer/Utility/Converters/FileConverter.cs
+++ AerotechMotorSizer/Utility/Converters/FileConverter.cs
@@ -3,9 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Interfaces;
+
 namespace Utility.Converters
 {
-    public class FileConverter
+    public class FileConverter : IConverter
     {
+        public bool HasPosition
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool HasVelocity
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool HasAcceleration
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public double[] Position
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public double[] Velocity
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public double[] Acceleration
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public double[] Time
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 }
